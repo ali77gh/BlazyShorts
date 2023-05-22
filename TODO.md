529c2e24c2c8cfb9e7387ff000220e44f8552c48
Ali ghahremani <alighahremani1377@gmail.com>
2023-05-22 10:04:56 +0330

    feature: simple shared state in backend todo: TODO.md file added with phases

---
+++ TODO.md
@@ -0,0 +1,24 @@
+phase 1: (backend)
+    0. save and load from RAM
+    1. add link
+    2. redirect to link
+    3. 404
+    4. modules
+
+phase 2: (backend)
+    1. add database
+    2. rate limiter middleware
+
+phase 3: (shared-lib)
+    1. protobuf and api models
+
+phase 4: (front-end)
+    1. simple landing page and routing
+    2. add link form
+
+phase 5: (backend, shared-lib)
+    1. login
+    2. manage links
+
+phase 6: (forntend)
+    1. login and dashboard UI
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,34 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router,extract::State};
 use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone)]
+struct AppState {
+    counter: Arc<Mutex<u32>>
+}
+
+impl AppState{
+
+    fn new() -> Self{
+        Self { counter: Arc::new(Mutex::new(0u32.to_owned())), }
+    }
+
+    fn get(&mut self) -> u32{
+        let mut data = self.counter.lock().expect("mutex was poisoned");
+        *data += 1;
+        *data
+    }
+}
+
 
 #[tokio::main]
 async fn main() {
 
-    let app = Router::new().route("/", get(handler));
+    let state = AppState::new();
+    let app = Router::new()
+        .route("/", get(handler))
+        .with_state(state);
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     axum::Server::bind(&addr)
@@ -13,6 +37,10 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> &'static str {
-    "Hello, world!"
+
+
+async fn handler(
+    State(mut state): State<AppState>,
+) -> String {
+    format!("hello for {}th time", state.get())
 }
