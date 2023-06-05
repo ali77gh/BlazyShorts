604932cbd7f2d0d813981e32f8f3045a59d71570
Ali Ghahremani <alighahremani1377@gmail.com>
2023-06-05 18:48:11 +0330

    feature: info component

--- webapp/src/app/components/home.rs
+++ webapp/src/app/components/home.rs
@@ -18,23 +18,46 @@ pub fn HomePage(cx: Scope, path: &'static str) -> impl IntoView {
     };
     
     let styles = style!(
+        .background {
+            background-color: #5B6C5D;
+            border-radius: 5px;
+        }
         .title {
-          background-color: red;
+          font-size: 50px;
           color: white;
-          font-size: 30px;
+          margin: 20px;
+        }
+        .tabs {
+            background-color: #59C9A5;
+            width: 100%;
+            height: 30px;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+        }
+        .tab_item {
+            padding: 10px;
+            color: white;
+            text-decoration: none;
+            background-color: #465775;
+        }
+        .divader {
+            background-color: #FFFFFF;
+            width: 1px;
         }
     );
 
 
     styled::view! { cx, styles,
-        <div class="title">"Blazy Shorts"</div>
-        {layout}
+        <div class="background">
+            <div class="title">"Blazy Shorts"</div>
+            <div class="tabs">
+                <a class="tab_item" href="/links">"links"</a>
+                <div class="divader"></div>
+                <a class="tab_item" href="/info">"info"</a>
+            </div>
+            <div> {layout} </div>
+        </div>
     }
-}
-
-
-
-#[component]
-pub fn Tabs(cx: Scope) -> impl IntoView {
-
 }
